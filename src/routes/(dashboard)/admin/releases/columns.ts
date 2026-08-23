cc7383e189e7f6fd800250477a521ab74caf0abf
Harsh Narayan Jha <harshnj@proton.me>
2026-08-23 15:41:59 +0530

    fix: release notes width

--- src/routes/(dashboard)/admin/releases/columns.ts
+++ src/routes/(dashboard)/admin/releases/columns.ts
@@ -1,5 +1,19 @@
+import { renderSnippet } from "@/lib/components/ui/data-table"
+import { DateTime } from "luxon"
+import { createRawSnippet } from "svelte"
+
 import type { AppRelease } from "@/lib/types"
 import type { ColumnDef } from "@tanstack/table-core"
+
+// Truncate helper snippet
+const truncatedCellSnippet = createRawSnippet<[{ text: string; maxWidthClass?: string }]>(
+  (getProps) => {
+    const { text, maxWidthClass = "max-w-[350px]" } = getProps()
+    return {
+      render: () => `<div class="${maxWidthClass} truncate" title="${text}">${text}</div>`
+    }
+  }
+)
 
 export const columns: ColumnDef<AppRelease>[] = [
   {
@@ -17,6 +31,22 @@ export const columns: ColumnDef<AppRelease>[] = [
   {
     accessorKey: "releaseNotes",
     header: "Release Notes",
-    cell: (info) => info.getValue()
+    cell: ({ row }) => {
+      return renderSnippet(truncatedCellSnippet, { text: row.original.releaseNotes })
+    }
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+    cell: ({ row }) => {
+      return row.original.createdAt.toLocaleString(DateTime.TIME_SIMPLE)
+    }
+  },
+  {
+    accessorKey: "updatedAt",
+    header: "Updated At",
+    cell: ({ row }) => {
+      return row.original.createdAt.toLocaleString(DateTime.TIME_SIMPLE)
+    }
   }
 ]
